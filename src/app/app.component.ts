44ea832341a75d3b2408854f53e017acdd1429bf
Katarzyna Wicherska <kawi@gft.com>
2017-02-02 09:59:25 +0100

    example of different counters, TODO: review zone.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cokolwiek!';
   counter = 0;
+  counter2 = 0;
+
+  constructor(){
+    setInterval(() => {
+      this.counter++;
+    }, 500)
+ 
+    setInterval(() => {
+      this.counter2++;
+    }, 1200)
+  }
 }
