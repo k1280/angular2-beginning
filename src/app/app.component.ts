9d8413c71d681c0707c536b9f834ce1b65f93d20
Katarzyna Wicherska <kawi@gft.com>
2017-02-02 14:54:52 +0100

    some changes based on page, css is a copy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,19 @@
 import { Component } from '@angular/core';
-
 export class Hero {
   id: number;
   name: string;
 }
-
 @Component({
   selector: 'my-app',
-  template: ` 
-  <h1>{{title}}</h1>
-  <h2>{{hero.name}} details!</h2>
-  <div><label>id: </label>{{hero.id}}</div>
-  <div>
-  <label>name: </label>
-  <input value="{{hero.name}}" placeholder="name">
-  </div>
-  `,
-  //templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -25,15 +21,4 @@ export class AppComponent {
     id: 1,
     name: 'Windstorm'
   };
-  counter2 = 0;
-
-  // constructor(){
-  //   setInterval(() => {
-  //     this.counter++;
-  //   }, 500)
-
-  //   setInterval(() => {
-  //     this.counter2++;
-  //   }, 1200)
-  // }
 }
