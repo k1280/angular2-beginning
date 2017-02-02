50988adce079b3397755a60ed015cf1d210efc94
Katarzyna Wicherska <kawi@gft.com>
2017-02-02 14:30:39 +0100

    change from app-root to my-app, heroes tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'my-app',
+  template: ` 
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+  <label>name: </label>
+  <input value="{{hero.name}}" placeholder="name">
+  </div>
+  `,
+  //templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Rent Payment Adminitration Application';
-  counter = 0;
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
   counter2 = 0;
 
   // constructor(){
   //   setInterval(() => {
   //     this.counter++;
   //   }, 500)
- 
+
   //   setInterval(() => {
   //     this.counter2++;
   //   }, 1200)
