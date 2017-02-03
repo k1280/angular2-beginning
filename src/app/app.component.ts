809e3e1b795a35d512019f5aed404f98da81f278
Katarzyna Wicherska <kawi@gft.com>
2017-02-03 09:09:13 +0100

    added styling in the template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,20 @@ export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Bombasto' },
+  { id: 13, name: 'Celeritas' },
+  { id: 14, name: 'Batmanno' },
+  { id: 15, name: 'PoohWooh' },
+  { id: 16, name: 'Wookie' },
+  { id: 17, name: 'Dr IQ' },
+  { id: 18, name: 'Magma' },
+  { id: 19, name: 'Tornado' },
+  { id: 20, name: 'Sunset' }
+];
+
 @Component({
   selector: 'my-app',
   template: `
@@ -19,7 +33,56 @@ export class Hero {
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    `
+    `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -29,15 +92,4 @@ export class AppComponent {
   };
   heroes = HEROES;
 }
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Bombasto' },
-  { id: 13, name: 'Celeritas' },
-  { id: 14, name: 'Batmanno' },
-  { id: 15, name: 'PoohWooh' },
-  { id: 16, name: 'Wookie' },
-  { id: 17, name: 'Dr IQ' },
-  { id: 18, name: 'Magma' },
-  { id: 19, name: 'Tornado' },
-  { id: 20, name: 'Sunset' }
-];
+
